d5231b88d9c66d5ae0201cc42a3829b34ecd8b0e
GioMench <giorgiaglm@gmail.com>
2024-03-13 17:56:17 +0100

    create array of to do list create function addToDo for push in array by input

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,36 @@ createApp({
         return {
             title: 'To Do List',
             checked: true,
-
+            newToDo : '',
             items: [
                 {
-                    text: '',
-                    
+                    text : 'portare il cane dal veterinario',
+                    done: false
+                },
+                {
+                    text : 'comprare gli ingredienti per la torta',
+                    done: false
+                },
+                {
+                    text : 'comprare gli ingredienti per la torta',
+                    done: false
+                },
+                
 
-                }]
+
+
+            ]
+        }
+    },
+    methods :{
+        addToDo(){
+            const newItem={
+                text: this.newToDo,
+               done: false
+            }
+            this.items.push(newItem);
+            this.newToDo = ''
         }
     }
+    
 }).mount('#app')
